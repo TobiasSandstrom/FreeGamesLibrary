1b10d25a13f2b4c244077dedbb3c2949fa933247
Tobias Sandstrom <tobias.sandstrom@mjukvarufabriken.se>
2025-01-20 13:25:04 +0100

    created state for the ui

---
+++ FreeGamesLibrary/Store/UiUseState/UiState.cs
@@ -0,0 +1,20 @@
+﻿using Fluxor;
+using FreeGamesLibrary.Data.Models;
+
+namespace FreeGamesLibrary.Store.UiUseState
+{
+
+    [FeatureState]
+    public record UiState
+    {
+        // Save all games in this list
+        public List<GameSimpleDisplayModel> GamesSimple { get; init; } = new List<GameSimpleDisplayModel>();
+
+
+
+        public List<GameSimpleDisplayModel> FeaturedGamesSimple { get; init; } = new List<GameSimpleDisplayModel>();
+
+
+        private UiState() { } // Required for creating initial state
+    }
+}

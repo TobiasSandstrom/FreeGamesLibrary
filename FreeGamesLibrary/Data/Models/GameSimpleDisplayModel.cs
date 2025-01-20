2ef96a693984fa0bcc6091703d73bca70cd1de36
Tobias Sandstrom <tobias.sandstrom@mjukvarufabriken.se>
2025-01-20 13:14:05 +0100

    Model for displaying simple games

---
+++ FreeGamesLibrary/Data/Models/GameSimpleDisplayModel.cs
@@ -0,0 +1,17 @@
+﻿namespace FreeGamesLibrary.Data.Models
+{
+    public class GameSimpleDisplayModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Thumbnail { get; set; } = string.Empty;
+        public string Short_Description { get; set; } = string.Empty;
+        public string Game_Url { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public string[] Platform { get; set; } = [];
+        public string Publisher { get; set; } = string.Empty;
+        public string Developer { get; set; } = string.Empty;
+        public string Release_Date { get; set; } = string.Empty;
+        public string FreeToGame_Profile_Url { get; set; } = string.Empty;
+    }
+}

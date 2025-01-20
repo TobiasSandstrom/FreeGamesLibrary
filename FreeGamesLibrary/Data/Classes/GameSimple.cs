3cd4ee8f604c28576c9b62b1ac6fc3bebe6c2a65
Tobias Sandstrom <tobias.sandstrom@mjukvarufabriken.se>
2025-01-20 13:16:12 +0100

    Game simple class that reflects api:s response

---
+++ FreeGamesLibrary/Data/Classes/GameSimple.cs
@@ -0,0 +1,17 @@
+﻿namespace FreeGamesLibrary.Data.Classes
+{
+    public class GameSimple
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Thumbnail { get; set; } = string.Empty;
+        public string Short_Description { get; set; } = string.Empty;
+        public string Game_Url { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public string Platform { get; set; } = string.Empty;
+        public string Publisher { get; set; } = string.Empty;
+        public string Developer { get; set; } = string.Empty;
+        public string Release_Date { get; set; } = string.Empty;
+        public string FreeToGame_Profile_Url { get; set; } = string.Empty;
+    }
+}

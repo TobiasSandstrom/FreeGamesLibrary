840440c393f255ca1f0244b0529d5076afad2ac0
Tobias Sandstrom <tobias.sandstrom@mjukvarufabriken.se>
2025-02-04 13:05:26 +0100

    Started Randomize Game. Need to think this over more thoroughly for it to work with state.

---
+++ FreeGamesLibrary/Store/UiUseState/Actions/RandomizeGameAction.cs
@@ -0,0 +1,17 @@
+﻿using FreeGamesLibrary.Data.Models;
+
+namespace FreeGamesLibrary.Store.UiUseState
+{
+    public class RandomizeGameAction
+    {
+        public List<GameSimpleDisplayModel> Games { get; set; }
+        public List<GameDetailedDisplayModel> DetailedGames { get; set; }
+
+        public RandomizeGameAction(List<GameSimpleDisplayModel> games, List<GameDetailedDisplayModel> detailedGames)
+        {
+            Games = games;
+            DetailedGames = detailedGames;
+        }
+
+    }
+}
---
+++ FreeGamesLibrary/Store/UiUseState/Effects/RandomizeGameEffect.cs
@@ -0,0 +1,33 @@
+﻿using Fluxor;
+using FreeGamesLibrary.Data.Models;
+
+namespace FreeGamesLibrary.Store.UiUseState.Effects
+{
+    public partial class Effects
+    {
+        [EffectMethod]
+        public async Task RandomizeGameEffect(RandomizeGameAction action, IDispatcher dispatcher)
+        {
+
+            Random _random = new Random();
+            var randomGame = action.Games.OrderBy(g => _random.Next()).Take(1);
+
+            if (randomGame.FirstOrDefault() != null)
+            {
+                if (action.DetailedGames.Any(x => x.Id == randomGame.FirstOrDefault().Id))
+                {
+                    dispatcher.Dispatch(new SetDetailedGameToDisplayAction(action.DetailedGames.FirstOrDefault(x => x.Id == randomGame.First().Id)));
+                }
+                else {
+                    var fetchedRandomGame = await GameService.GetGameDetailedAsync(randomGame.FirstOrDefault().Id);
+                    dispatcher.Dispatch(new SetDetailedGameToDisplayAction(fetchedRandomGame));
+
+                }
+
+            }
+
+            dispatcher.Dispatch(new SetDetailedGameToDisplayAction(new GameDetailedDisplayModel()));
+
+        }
+    }
+}
